421c36a56e0cc884ad2e1eb9d8ae0cf274156d93
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-12 16:14:07 -0700

    The input data is verified in the back end logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,25 @@
 $(document).ready(function(){
-  $("form#trackSuggester").$(function(){
-    var representInput = $("input:checkbox[name=representYou]:checked").val();
-    var personalityInput = $()
+  $("form#trackSuggester").submit(function(event){
 
+    var representInput = $("input:checkbox[name=representYou]:checked").val();
+    var personalityInput = $("input:image[name=imageButton]:checked").val();
+    var environmentInput = $("input:radio[name=environment]:checked").val();
+    var workingInput = $("select.form-control").val();
+    var industryInput = $("input:radio[name=industry]:checked").val();
+    var wageInput = $("input:radio[name=wages]:checked").val();
 
     $("#representOutput").text(representInput);
-    $("#personalityOutput").text(personalityInput)
+    $("#personalityOutput").text(personalityInput);
+    $("#environmentOutput").text(environmentInput);
+    $("#projectOutput").text(workingInput);
+    $("#industryOutput").text(industryInput);
+    $("#wageOutput").text(wageInput);
 
 
-  })
+    event.preventDefault();
+// closing trackSuggester function.
+  });
 
 
-
-})
+// closing readyfunction.
+});
