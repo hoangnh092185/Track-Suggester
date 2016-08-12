e1307ceb2d15382061761a2fe16d7683ead9931f
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-12 14:05:10 -0700

    Connect the question 2 input to the business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,11 @@
 $(document).ready(function(){
-
-  $("button#photo").click(function() {
-    $("#imgOutput1").apprend("#showPhoto1");
-  });
-
-  $("button#photoButton2").click(function() {
-
-    $("#imgOutput1").addClass("#showPhoto2");
-  });
-
-  $("button#photoButton3").click(function() {
-    $("#imgOutput1").removeClass();
-    $("#imgOutput1").addClass("#showPhoto3");
-  });
+  $("form#trackSuggester").$(function(){
+    var representInput = $("input:checkbox[name=representYou]:checked").val();
+    var personalityInput = $()
 
 
-  $("form-group").$(function(){
-    var infoInput1 = $("question1");
-    var infoInput2 = $("question2");
-    var infoInput3 = $("question3");
-    var infoInput4 = $("question4");
-    var infoInput5 = $("question5");
-    var infoInput6 = $("question6");
+    $("#representOutput").text(representInput);
+    $("#personalityOutput").text(personalityInput)
 
 
   })
