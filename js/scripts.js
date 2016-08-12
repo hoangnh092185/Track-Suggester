5bfcf91feb62de21ef2e096d67c56fb76e1e4988
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-12 08:25:16 -0700

    Create a barebone outline servey that would direct user to a software languages.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(fucntion(){
+  $("form-group").$(function(){
+    var infoInput1 = ("question1");
+    var infoInput2 = ("question2");
+    var infoInput3 = ("question3");
+    var infoInput4 = ("question4");
+    var infoInput5 = ("question5");
+    var infoInput6 = ("question6");
+
+  })
+})
