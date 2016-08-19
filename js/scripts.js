d5c383fe2f9774dde72f329353fee3bf138c1543
Nhat Hoang <hoangnh092185@gmail.com>
2016-08-19 11:44:17 -0700

    Counts all the input values, c,t and d

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,88 @@
+var wordsHolder1;
+var wordsHolder2;
+var wordsHolder3;
+var wordsHolder5;
+var wordsHolder6;
+var resultHolder;
+var resultHolder1 = [];
+var count1;
+var count2;
+var count3;
+var countHolder;
+function wordsHolder1(input) {
+  if (input == "coding"){
+    wordsHolder1 = "c";
+    resultHolder1.push("C")
+  }else if (input == "design"){
+    wordsHolder1 = "d";
+    resultHolder1.push("D")
+  }else {
+    wordsHolder1 = "t";
+    resultHolder1.push("T")
+  }
+}
+function wordsHolder2(input) {
+  if (input == "bold"){
+    wordsHolder2 = "c";
+    resultHolder1.push("C")
+  }else if (input == "creative"){
+    wordsHolder2 = "d";
+    resultHolder1.push("D")
+  }else {
+    wordsHolder2 = "t";
+    resultHolder1.push("T")
+  }
+}
+function wordsHolder3(input) {
+  if (input == "in a business environments"){
+    wordsHolder3 = "c";
+    resultHolder1.push("C")
+  }else if (input == "at home"){
+    wordsHolder3 = "d";
+    resultHolder1.push("D")
+  }else {
+    wordsHolder3 = "t";
+    resultHolder1.push("T")
+  }
+}
+
+function wordsHolder5(input) {
+  if (input == "big company"){
+    wordsHolder5 = "c";
+    resultHolder1.push("C")
+  }else if (input == "yourself"){
+    wordsHolder5 = "d";
+    resultHolder1.push("D")
+  }else {
+    wordsHolder5 = "t";
+    resultHolder1.push("T")
+  }
+}
+
+function wordsHolder6(input) {
+  if (input == "low risk and low wages"){
+    wordsHolder6 = "c";
+    resultHolder1.push("C")
+  }else if (input == "Average risk and medium wages"){
+    wordsHolder6 = "d";
+    resultHolder1.push("D")
+  }else {
+    wordsHolder6 = "t";
+    resultHolder1.push("T")
+  }
+}
+console.log(resultHolder1);
+
+
+// var patt1 = /[C]/g;
+// var count1 = resultOutput.match(patt1);
+// var patt2 = /[D]/g;
+// var count2 = resultOutput.match(patt2);
+// var patt3 = /[T]/g;
+// var count3 = resultOutput.match(patt3);
+// console.log(count1 + " " + count2 + " " + count3);
+
+
 $(document).ready(function(){
   $("form.trackSuggester").submit(function(event){
 
@@ -8,14 +93,22 @@ $(document).ready(function(){
     var industryInput = $("input:radio[name=industry]:checked").val();
     var wageInput = $("input:radio[name=wages]:checked").val();
 
-    if (representInput === "coding"){
-      $("#trackResult").text("PHP/Drupal");
+    wordsHolder1(representInput);
+    wordsHolder2(personalityInput);
+    wordsHolder3(environmentInput);
+    wordsHolder5(industryInput);
+    wordsHolder6(wageInput);
 
-    }else if (representInput === "design"){
-      $("#trackResult").text("CSS/Design");
-    }else {
-      $("#trackResult").text("Ruby/Rail");
-    }
+    resultOutput = (wordsHolder1 + wordsHolder2 + wordsHolder3 + wordsHolder6 + wordsHolder6);
+    console.log(resultOutput);
+
+      var patt1 = /[c]/g;
+      var count1 = resultOutput.match(patt1);
+      var patt2 = /[d]/g;
+      var count2 = resultOutput.match(patt2);
+      var patt3 = /[t]/g;
+      var count3 = resultOutput.match(patt3);
+    console.log(count1 + " " + count2 + " " + count3);
 
     $("#representOutput").text(representInput);
     $("#personalityOutput").text(personalityInput);
@@ -27,6 +120,5 @@ $(document).ready(function(){
     $(".resultText").show();
     $("#showTrImg2").show();
     event.preventDefault();
-
   });
 });
